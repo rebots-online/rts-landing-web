e2ad92bdeb9d174a09b8787ed6bbec53dc8e40c3
Robin Cheung, MBA <robin@robin.mba>
2026-07-18 09:19:29 -0400

    Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: process.env.GITHUB_ACTIONS ? "/rts-landing-web/" : "/",
   build: {
     outDir: "dist",
     sourcemap: true,
